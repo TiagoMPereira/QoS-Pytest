55eac30940388495754b457e55fc9f0534aadd51
BernardoGandolpho <begandolpho@gmail.com>
2022-12-06 13:31:06 -0300

    tests: include more tests

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -20,6 +20,28 @@ def test_get_method__general_url__expect_first_response():
     assert response.status_code == status
     assert result[0] ==  expected_result
 
+
+def test_get_method_by_id__general_url__expect_first_response():
+    # FIXTURE
+    id = 1
+    url = f"https://jsonplaceholder.typicode.com/"
+    status = 200
+    expected_result = {
+        "userId": 1,
+        "id": 1,
+        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    }
+
+    # EXERCISE
+    response = requests.get(url+f"posts/{id}")
+
+    # ASSERT
+    result = response.json()
+    assert response.status_code == status
+    assert result ==  expected_result
+
+
 def test_post_method__general_url__expect_post_body():
     # FIXTURE
     url = "https://jsonplaceholder.typicode.com/"
@@ -36,16 +58,17 @@ def test_post_method__general_url__expect_post_body():
     # ASSERT
     result = response.json()
     assert response.status_code == status
-    assert result["title"] ==  body["title"]
-    assert result["body"] ==  body["body"]
-    assert result["userId"] ==  body["userId"]
+
+    del(result["id"])
+    assert result == body
+
 
 def test_put_method__general_url__expect_update_body():
     # FIXTURE
     url = "https://jsonplaceholder.typicode.com/"
     status = 200
     body = {
-        "id": 101,
+        "id": 1,
         "title": 'Atualização - PUT',
         "body": 'Novo body',
         "userId": 1,
@@ -57,9 +80,9 @@ def test_put_method__general_url__expect_update_body():
     # ASSERT
     result = response.json()
     assert response.status_code == status
-    assert result["title"] ==  body["title"]
-    assert result["body"] ==  body["body"]
-    assert result["userId"] ==  body["userId"]
+    
+    assert result == body
+
 
 def test_delete_method__general_url__expect_status_code():
     # FIXTURE
@@ -71,3 +94,20 @@ def test_delete_method__general_url__expect_status_code():
 
     # ASSERT
     assert response.status_code == status
+    assert response.json() == {}
+
+
+def test_get_method_invalid_id__general_url__expect_first_response():
+    # FIXTURE
+    id = "invalid_id"
+    url = f"https://jsonplaceholder.typicode.com/"
+    status = 404
+    expected_result = {}
+
+    # EXERCISE
+    response = requests.get(url+f"posts/{id}")
+
+    # ASSERT
+    result = response.json()
+    assert response.status_code == status
+    assert result ==  expected_result
